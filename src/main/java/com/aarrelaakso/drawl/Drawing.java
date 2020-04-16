a92a02171049040367e7807f788a52253d8b10a6
Unknown <aarre.laakso@gmail.com>
2020-04-16 18:32:09 -0400

    More BigDecimal bugs - Passes 75 of 75 tests

--- src/main/java/com/aarrelaakso/drawl/Drawing.java
+++ src/main/java/com/aarrelaakso/drawl/Drawing.java
@@ -34,25 +34,34 @@ public class Drawing {
     /**
      * Get the SVG for this Drawing.
      *
-     * @param drawingWidth The desired width of the output (in pixels).
-     * @param drawingHeight The desired height of the output (in pixels).
-     * @return A string of valid SVG that depicts the drawing within the bounds of x and y.
+     * @param drawingWidth  The desired width of the output;
+     *                      the type is Float to match the SVG spec for numeric precision.
+     * @param drawingHeight The desired height of the output;
+     *                      the type is Float to match the SVG spec for numeric precision.
+     * @return A string of valid SVG that depicts the drawing within the bounds of drawingWidth and drawingHeight.
      */
-    public String getSVG(BigDecimal drawingWidth, BigDecimal drawingHeight) {
+    public String getSVG(Float drawingWidth, Float drawingHeight) {
         this.setExplicitWidth(drawingWidth);
         this.setExplicitHeight(drawingHeight);
         return this.getSVG();
     }
 
+    /**
+     * Get the SVG for this drawing.
+     *
+     * @param drawingWidth The desired width of the output; the type is Integer to allow for common use cases.
+     * @param drawingHeight The desired height of the output; the type is Integer to allow for common use cases.
+     * @return A string of valid SVG that depicts the drawing within the bounds of drawingWidth and drawingHeight.
+     */
     public String getSVG(Integer drawingWidth, Integer drawingHeight) {
-        this.setExplicitWidth(new BigDecimal(drawingWidth));
-        this.setExplicitHeight(new BigDecimal(drawingHeight));
+        this.setExplicitWidth(drawingWidth.floatValue());
+        this.setExplicitHeight(drawingHeight.floatValue());
         return this.getSVG();
     }
 
     /**
      * Get the SVG for this Drawing
-     *
+     * <p>
      * Assumes that the explicit width and height have been set
      *
      * @return A string of valid SVG that depicts the drawing within the bounds of the explicit width and height
@@ -98,14 +107,14 @@ public class Drawing {
             }
         }
         svg += "</svg>";
-        return(svg);
+        return (svg);
     }
 
     public BigDecimal getExplicitHeight() {
         return this.explicitHeight;
     }
 
-    public BigDecimal getExplicitWidth(){
+    public BigDecimal getExplicitWidth() {
         return this.explicitWidth;
     }
 
@@ -137,7 +146,7 @@ public class Drawing {
             }
             Circle objectToLeft = content.getRightOf();
             if (objectToLeft != null) {
-                    width = width.add(objectToLeft.getImplicitWidth());
+                width = width.add(objectToLeft.getImplicitWidth());
             }
             count++;
         }
@@ -156,7 +165,7 @@ public class Drawing {
             if (count == 0) {
                 implicitWidthOfContents = implicitWidthOfContents.add(content.getImplicitWidth());
             } else {
-                if ((content.getRightOf() != null)  || (content.getLeftOf() != null)) {    // TODO: What if we are counting twice left and right?
+                if ((content.getRightOf() != null) || (content.getLeftOf() != null)) {    // TODO: What if we are counting twice left and right?
                     implicitWidthOfContents = implicitWidthOfContents.add(content.getImplicitWidth());
                 }
             }
@@ -169,19 +178,21 @@ public class Drawing {
     /**
      * Set the explicit height of this Drawing
      *
-     * @param height the explicit width of the Drawing
+     * @param height the explicit width of the Drawing; it is a Float to match
+     *               the precision allowed by the SVG spec.
      */
-    public void setExplicitHeight(BigDecimal height) {
-        this.explicitHeight = height;
+    public void setExplicitHeight(Float height) {
+        this.explicitHeight = BigDecimal.valueOf(height);
     }
 
     /**
      * Set the explicit width of this Drawing
      *
-     * @param width the explicit width of the Drawing, in pixels
+     * @param width the explicit width of the Drawing; it is a Float
+     *              to match the precision allowed by the SVG spec.
      */
-    public void setExplicitWidth(BigDecimal width) {
-        this.explicitWidth = width;
+    public void setExplicitWidth(Float width) {
+        this.explicitWidth = BigDecimal.valueOf(width);
         BigDecimal implicitWidthOfContents = this.getImplicitWidthOfContents();
         if (implicitWidthOfContents.compareTo(BigDecimal.ZERO) != 0) {
             BigDecimal explicitWidthPerImplicitWidth = this.explicitWidth.divide(implicitWidthOfContents, BigDecimalMath.mathContext);
