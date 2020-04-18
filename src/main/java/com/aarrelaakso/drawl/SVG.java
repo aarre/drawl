a2bbab42745e17bec30ca6c365e04b3ee80032d1
Unknown <aarre.laakso@gmail.com>
2020-04-17 21:19:52 -0400

    Use floating point numbers in SVG output - Passes 77 of 77 tests

--- src/main/java/com/aarrelaakso/drawl/SVG.java
+++ src/main/java/com/aarrelaakso/drawl/SVG.java
@@ -7,17 +7,19 @@ import java.math.BigDecimal;
 public class SVG {
 
     /**
-     * Convert a number to a string with as few decimal points as necessary.
+     * Convert a number to a string for SVG.
+     *
+     * The number will be represented with as many decimal points as necessary for SVG and no more than SVG can
+     * handle.
      *
      * @param number the number to convert.
      */
     public static String toString(BigDecimal number) {
-        String result = null;
-        // TODO Handle more than integers
+        String result;
         if (BigDecimalMath.isIntegerValue(number)) {
             result = String.valueOf(number.intValue());
         } else {
-            result = String.valueOf(number);
+            result = String.valueOf(number.floatValue());
         }
         return result;
     }
