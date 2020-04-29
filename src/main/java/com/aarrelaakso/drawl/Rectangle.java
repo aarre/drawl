9f584df86691cf57d8743721abcbc87d9fa91f54
Unknown <aarre.laakso@gmail.com>
2020-04-29 13:25:20 -0400

    Create constructors for asymmetric rectangles - Passes 186 of 186 tests

--- src/main/java/com/aarrelaakso/drawl/Rectangle.java
+++ src/main/java/com/aarrelaakso/drawl/Rectangle.java
@@ -2,6 +2,42 @@ package com.aarrelaakso.drawl;
 
 public class Rectangle extends Shape {
 
+    private final SisuBigDecimal height;
+    private final SisuBigDecimal width;
+
+    /**
+     * Creates a new Rectangle with the default width and height.
+     */
+    public Rectangle()
+    {
+        this.height = SisuBigDecimal.ONE;
+        this.width = SisuBigDecimal.ONE;
+    }
+
+    /**
+     * Creates a new Rectangle with a given width and height.
+     *
+     * @param implicitWidth the width of the new Rectangle.
+     * @param implicitHeight the height of the new Rectangle.
+     */
+    public Rectangle(Integer implicitWidth, Integer implicitHeight)
+    {
+        this.height = SisuBigDecimal.valueOf(implicitHeight);
+        this.width = SisuBigDecimal.valueOf(implicitWidth);
+    }
+
+    /**
+     * Creates a new Rectangle with a given width and height.
+     *
+     * @param implicitWidth the width of the new Rectangle.
+     * @param implicitHeight the height of the new Rectangle.
+     */
+    public Rectangle(SisuBigDecimal implicitWidth, SisuBigDecimal implicitHeight)
+    {
+        this.height = implicitHeight;
+        this.width = implicitWidth;
+    }
+
     public String getSVG() {
         if (this.getExplicitWidth() == null || this.getExplicitHeight() == null)
         {
