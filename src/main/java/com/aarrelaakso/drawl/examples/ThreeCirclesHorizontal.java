3872178a6dfb99ae4fb2ef548c8ebf460f9afc12
Unknown <aarre.laakso@gmail.com>
2020-05-01 21:24:42 -0400

    Modify three circles example

--- src/main/java/com/aarrelaakso/drawl/examples/ThreeCirclesHorizontal.java
+++ src/main/java/com/aarrelaakso/drawl/examples/ThreeCirclesHorizontal.java
@@ -12,72 +12,21 @@ package com.aarrelaakso.drawl.examples;
 
 import com.aarrelaakso.drawl.Circle;
 import com.aarrelaakso.drawl.Drawing;
-import com.aarrelaakso.drawl.Shape;
 
 import java.io.IOException;
 
-/** \mainpage Drawl Documentation
- *
- * \section intro_sec Introduction
- *
- * Drawl is a drawing language. That means that it allows you to represent drawings
- * as programs, then render the programs into images.
- *
- * \section examples_sec Examples
- *
- * As an example, the following code:
- *
- * ~~~{.java}
- * public class Drawl {
- *     public static void main(String[] args) {
- *         System.out.println("Hello World");
- *         Drawing drawing = new Drawing();
- *         Circle circle1 = new Circle();
- *         drawing.add(circle1);
- *         Circle circle2 = new Circle();
- *         drawing.add(circle2);
- *         circle2.setRightOf(circle1);
- *         String svg = drawing.getSVG(200, 100);
- *         System.out.println(svg);
- *     }
- * }
- * ~~~
- *
- * Renders a drawing showing two adjacent circles:
- *
- * <IMG SRC="../example1.svg" ALIGN="left" WIDTH="200" HEIGHT="100" />
- *
- * <BR CLEAR="left" />
- *
- * <P><P>This simple example illustrates the basic idea, but it can be extended to create many beautiful and useful drawings.
- *
- *
- *
- *
- *
- *
- */
-
 public class ThreeCirclesHorizontal
 {
-
-    /**
-     * Main method for integration tests.
-     *
-     * @param args
-     * @throws IOException
-     */
     public static void main(String[] args) throws IOException {
         Drawing drawing = new Drawing();
-        Shape shape1 = new Circle();
-        drawing.add(shape1);
-        Shape shape2 = new Circle();
-        drawing.add(shape2);
-        shape2.setRightOf(shape1);
-        Shape shape3 = new Circle();
-        drawing.add(shape3);
-        shape3.setRightOf(shape2);
-        drawing.writeToFile("src/main/java/com/aarrelaakso/drawl/examples/ThreeCirclesHorizontal.svg", 100, 100);
+        Circle circle1 = new Circle();
+        drawing.add(circle1);
+        Circle circle2 = new Circle();
+        drawing.add(circle2);
+        circle2.setRightOf(circle1);
+        Circle circle3 = new Circle();
+        drawing.add(circle3);
+        circle3.setRightOf(circle2);
+        drawing.writeToFile("src/main/java/com/aarrelaakso/drawl/examples/ThreeCirclesHorizontal.svg", 200, 200);
     }
-
 }
